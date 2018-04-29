04e11a8b87667ea881c95b95a07c9cc6e98d10e5
KentonDunn <kenton.dunn@gmail.com>
2018-04-29 16:25:41 -0400

    I keep getting an error that I don't know how to fix. I've googled, but it's beyond me.  Frustrated and might have to end here. Will ask a TA or my tutor for help next class

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -37,6 +37,7 @@ $("#addTrain").on("click", function(event) {
     dateAdded: firebase.database.ServerValue.TIMESTAMP
   };
 
+  //information being pushed to firebase.  Naming convention feels sloppy.
   database.ref().push(newTrain);
 
   console.log(newTrain.name);
@@ -60,11 +61,37 @@ database
       // storing the snapshot.val() in a variable for convenience
       var sv = snapshot.val();
 
-      // Console.loging the last user's data
-      console.log(sv.name);
-      console.log(sv.destination);
-      console.log(sv.train);
-      console.log(sv.freq);
+      // Console.logging the last train entered
+      console.log("Train Name: " + sv.name);
+      console.log("Destination:" + sv.destination);
+      console.log("First Train:" + sv.train);
+      console.log("Frequency:" + sv.freq + " minutes");
+
+      //going back 1 year to make sure the first train has arrived prior
+      //to the current time
+
+      var yearAgoTrain = moment(sv.train, "HH:mm").subtract(1, "years");
+      console.log(yearAgoTrain);
+
+      //setting up the current time for future calculation
+      var currentTime = moment();
+      console.log("Current time: " + moment(currentTime).format("HH:mm"));
+
+      //time difference between first train running and current time in minutes
+      //this will be a very large number, but the computer can calculate quickly
+
+      var timeDifference = moment().diff(moment(yearAgoTrain), "minutes");
+      console.log("Difference in Time Since First Train: " + timeDifference);
+
+      /*/calculate the remainder modulo to calculate when the next train will arrive
+      var timeRemainder = timeDifference % sv.freq;
+      Console.log("HEY" + timeRemainder);*/
+
+      /*/calculate minutes until the next train
+      var minutesTillNextTrain = sv.freq - timeRemainder;
+      console.log(
+        "Minutes Until Next Train: " + minutesTillNextTrain + "minutes"
+      );*/
 
       $("#newSchedule").append(
         "<tr>" +
@@ -78,19 +105,13 @@ database
           sv.freq +
           "</td>" +
           "<td>" +
-          "EMPTY" +
+          "NEXT ARRIVAL TIME" +
           "</td>" +
           "<td>" +
           "EMPTY" +
           "</td>" +
           "</tr>"
       );
-
-      // Change the HTML to reflect
-      $("#name-display").text(sv.name);
-      $("#email-display").text(sv.email);
-      $("#age-display").text(sv.age);
-      $("#comment-display").text(sv.comment);
 
       // Handle the errors
     },
